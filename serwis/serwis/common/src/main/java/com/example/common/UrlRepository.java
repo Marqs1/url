55970c5f02716cffab64c3f475077063fe571c67
Marqs1 <93042274+Marqs1@users.noreply.github.com>
2025-06-16 10:17:54 +0200

    Delete serwis directory

--- serwis/serwis/common/src/main/java/com/example/common/UrlRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.common;
-
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UrlRepository extends CrudRepository<UrlMapping, String> {
-}
-
